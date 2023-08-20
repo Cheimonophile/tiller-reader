4bcc31ad9f791e25ae6a4f5c3e04161377641dcf
Benjamin McLemore <51517288+Chionophile@users.noreply.github.com>
2023-08-20 00:19:26 -0400

    Cumulative net

--- src/constants.py
+++ src/constants.py
@@ -6,3 +6,5 @@ PROJECT_DIR = CWD.parent
 TILLER_PATH = PROJECT_DIR / 'Tiller.xlsx'
 DB_PATH = PROJECT_DIR / 'banking-data.sqlite'
 TILLER_LOGS_DIR = PROJECT_DIR / 'tiller-logs'
+
+
--- src/tiller_etl.py
+++ src/tiller_etl.py
@@ -5,27 +5,8 @@ import datetime as dt
 from .constants import *
 
 
-def tiller_to_db():
-    transactions = pd.read_excel(TILLER_PATH, sheet_name='Transactions', parse_dates=['Date', 'Month','Week','Date Added'])
-    categories = pd.read_excel('../Tiller.xlsx', sheet_name='Categories')
-    transactions = transactions.merge(categories[['Category','Group','Type']], how='left')
-    transactions.columns = transactions.columns.str.lower().str.replace(' ', '_').str.replace('#','no')
-    transactions = transactions.set_index('transaction_id').reset_index()
-    transactions['amount'] = transactions['amount'].mul(100).astype(int)
-    transactions[['date', 'month', 'week', 'date_added']] = transactions[['date', 'month', 'week', 'date_added']].astype(str)
-    transactions.to_csv(TILLER_LOGS_DIR / f"tiller-{dt.date.today()}.csv", index=False)
-    with sqlite3.connect(DB_PATH) as conn:
-        sql = f"""INSERT OR REPLACE INTO transactions VALUES ({', '.join(f":{c}" for c in transactions.columns)});"""
-        rows = [
-            {c: row[c] for c in transactions.columns}
-            for _, row in transactions.iterrows()
-        ]
-        conn.executemany(sql, rows)
-
-
 def get_transactions() -> pd.DataFrame:
-    tiller_to_db()
-    with sqlite3.connect(DB_PATH) as conn:
-        df = pd.read_sql("SELECT * FROM transactions;", conn, parse_dates=['date', 'month','week','date_added'])
-        df['amount'] = df['amount'].div(100).round(2)
-    return df
+    transactions = pd.read_excel(TILLER_PATH, sheet_name='Transactions', parse_dates=['Date', 'Month','Week','Date Added'])
+    categories = pd.read_excel(TILLER_PATH, sheet_name='Categories')
+    transactions = transactions.merge(categories[['Category','Group','Type']], how='left')
+    return transactions
